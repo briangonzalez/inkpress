c2de4d709e19763d21f3c76fcd09e8269616853d
Brian Gonzalez <brian.gonzalez@geni.com>
2013-02-18 21:20:14 -0800

    added long expiring for static files

--- application/app/ink_app.rb
+++ application/app/ink_app.rb
@@ -57,6 +57,7 @@ class InkApp < Sinatra::Base
   before do
     # opt into the future
     response['X-UA-Compatible'] = "IE=edge,chrome=1"
+    expires 3600, :public, :must_revalidate
   end
 
   get '/' do
